0c1ec3c855597abaf49145e5e1666437bc7326dc
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-10 11:02:20 -0500

    searchfield

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,3 +1,26 @@
 import React, { Component } from 'react';
 
 // Class component
+
+class SearchField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: ''
+    };
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.update(e, this.state.searchText)}>
+        <input 
+          type="text" 
+          placeholder="Search GIFS" 
+          value={this.state.searchText} 
+          onChange={(e) => this.setState({searchText: e.target.value})} 
+        />
+        <input type="submit" value="Search" />
+      </form>
+    );
+  }
+}
