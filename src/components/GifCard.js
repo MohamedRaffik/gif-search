3ba52af663a174ee1731e170b5e8d8c85747c286
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-10 11:09:39 -0500

    -

--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 
-<<<<<<< HEAD
-//Display for gifs, functional component 
-=======
 var APIKey = 'ilp9bNjZCdFKtrxG2ctF9PUilDGuggtp';
 
 //Display for gifs, functional component 
@@ -16,4 +13,3 @@ const GifCard = (props) => {
 }
 
 export default GifCard;
->>>>>>> a9cec72e8caee0f5c48e9ba7a37e239141162999
