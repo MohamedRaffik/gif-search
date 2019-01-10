5fb8c5f4cf958a88c445fef9fd792abbac3d6b3e
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-10 13:54:14 -0500

    css on title and search

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1 class="title">GIF Search</h1>
         <SearchField update={this.Search} />
         <GifCard gifs={this.state.gifs} />
       </div>
