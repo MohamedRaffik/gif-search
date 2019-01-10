ff349dc6485440f6afdc2e4daad9d18504ffd30b
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-10 11:57:36 -0500

    loads gifs

--- api_key.js
+++
@@ -1,3 +0,0 @@
-API_KEY='ilp9bNjZCdFKtrxG2ctF9PUilDGuggtp'
-
-export default API_KEY;
--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 //Display for gifs, functional component 
 const GifCard = (props) => {
-    var gifs = props.gifs.map((element) => (<img src={element.url} height={element.height} width={element.width} />));
+    var gifs = props.gifs.map((element, index) => (<img key={index} alt="" src={element.url} height={element.height} width={element.width} />));
     return(
         <div>
             {gifs}
